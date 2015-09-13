c7e71a02aaef8276e85469dfb19c900631d473d0
redtoor <redtoor@live.com>
2015-09-13 14:37:55 -0500

    Added Build Info.
    Build Details

--- core/updatekatana.py
+++ core/updatekatana.py
@@ -8,20 +8,21 @@ from core import info
 import subprocess
 import socket
 import os
+
 def update():
 	print ""
 	print "     ["+colors.O+"!"+colors.W+"] Update"
-	print "     ["+colors.O+"!"+colors.W+"] Version Current : "+info.version
+	print "     ["+colors.O+"!"+colors.W+"] Version Current : "+info.version+", Build: "+info.build
 	try:
 		red=socket.socket(socket.AF_INET, socket.SOCK_STREAM)       
 		red.connect(("github.com", int(80))) 
 		if True:
-			print "     Downloading project"
+			print "     ["+colors.O+"!"+colors.W+"] Downloading project"
 			subprocess.Popen("cd /tmp;git clone https://github.com/RedToor/katana.git;cp -R /tmp/katana/* /usr/share/katana/;rm -rf /tmp/katana/*", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
 			subprocess.Popen("cd /usr/share/katana/core;python upgrade.py", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-			print "     Upgrading."
+			print "     ["+colors.O+"!"+colors.W+"] Upgrading."
 			pass
-			print "     ["+colors.G+"+"+colors.W+"] Katana framework Updated"
+			print "     ["+colors.G+"+"+colors.W+"] Katana framework was Updated"
 			print ""
 	except:
 		print("     ["+colors.R+"-"+colors.W+"] Error, No connneted to Internet. ")
