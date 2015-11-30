9306c126656272c660adbec2e47365f094b90a22
RedToor <09890@hotmail.com>
2015-11-29 19:06:01 -0500

    Update Build date and version core

--- core/info.py
+++ core/info.py
@@ -3,8 +3,8 @@
 # @Katana Information
 #
 
-version="0.0.0.7"
-date="29/03/2015 - 11/10/2015"
-modules="29"
+version="0.0.0.8"
+date="29/03/2015 - 30/11/2015"
+modules="30"
 tema="Warrior theme"
-build="0049"
+build="0050"
