12a4d4bcb3349c754991800ad2293b5be794a0e7
RedToor <09890@hotmail.com>
2015-11-29 19:12:11 -0500

    Added New module

--- ktf.linker.py
+++ ktf.linker.py
@@ -1,7 +1,8 @@
-#
-# Katana framework 
-# @Katana Linker
-#
+### Katana Framework Linker
+### you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation, either version 3 of the License, or
+### (at your option) any later version. 
 
 from core.Setting import *
 from core.design import *
@@ -10,6 +11,7 @@ from core import colors
 from core import info
 import getopt
 import sys
+
 CLASS_LINKER=DESIGN()
 options, remainder = getopt.getopt(sys.argv[1:], 'd:t:p:f:u:ps:m:s:c:z:x:v:q:h')
 for option, arg in options:
@@ -131,17 +133,88 @@ elif m == "set/gdreport":
 		CLASS_LINKER.space()
 	else:
 		GetDataReport.run(target=t, js=q)
-#ClientFTP.run("127.0.0.1","80","anonymous","password")
-#ClientMYSQL.run("127.0.0.1","3306","admin","admin")
-#ClientPOP3.run("127.0.0.1","110","admin","admin")
-#BruteForceSQL.run("127.0.0.1","3306","admin","admin")
-#BruteForceSSH.run("127.0.0.1","22","admin","admin")
+
+elif m == "clt/ftp":
+	if h==1:
+		print "\n python "+KTFLINKER+" -m clt/ftp -t target.com -p 21 -u root -ps toor\n"
+		print " -t [target]         IP or DNS target "
+		print " -p [port]           Port target"
+		print " -u [username]       Username"
+		print " -ps [password]      Password"
+		CLASS_LINKER.space()
+	else:
+		ClientFTP.run(target=t,port=p,username=u,password=ps)
+elif m == "clt/sql":
+	if h==1:
+		print "\n python "+KTFLINKER+" -m clt/sql -t target.com -p 3306 -u root -ps toor\n"
+		print " -t [target]         IP or DNS target "
+		print " -p [port]           Port target"
+		print " -u [username]       Username"
+		print " -ps [password]      Password"
+		CLASS_LINKER.space()
+	else:
+		ClientMYSQL.run(target=t,port=p,username=u,password=ps)
+elif m == "clt/pop3":
+	if h==1:
+		print "\n python "+KTFLINKER+" -m clt/pop -t target.com -p 110 -u root@target.com -ps toor\n"
+		print " -t [target]         IP or DNS target "
+		print " -p [port]           Port target"
+		print " -u [username]       Username"
+		print " -ps [password]      Password"
+		CLASS_LINKER.space()
+	else:
+		ClientPOP3.run(target=t,port=p,username=u,password=ps)
+elif m == "btf/sql":
+	if h==1:
+		print "\n python "+KTFLINKER+" -m btf/sql -t target.com -p 3306 -u root -d core/db/pass.dicc\n"
+		print " -t [target]         IP or DNS target "
+		print " -p [port]           Port target"
+		print " -u [username]       Username"
+		print " -d [dictionary]     Password Wordlist"
+		CLASS_LINKER.space()
+	else:
+		BruteForceSQL.run(target=t,port=p,username=u,dictionary=d)
+elif m == "btf/ssh":
+	if h==1:
+		print "\n python "+KTFLINKER+" -m btf/ssh -t target.com -p 22 -u root -d core/db/pass.dicc\n"
+		print " -t [target]         IP or DNS target "
+		print " -p [port]           Port target"
+		print " -u [username]       Username"
+		print " -d [dictionary]     Password Wordlist"
+		CLASS_LINKER.space()
+	else:
+		BruteForceSSH.run(target=t,port=p,username=u,dictionary=d)
+elif m == "net/lanlive":
+	if h==1:
+		print "\n python "+KTFLINKER+" -m net/lanlive -t 192.168.0.1\n"
+		print " -t [target]            Ip address of network to scan"
+		CLASS_LINKER.space()
+	else:
+		LANScanner.run(nets=t, types="null")
+elif m == "net/portscan":
+	if h==1:
+		print "\n python "+KTFLINKER+" -m net/portscan -t 192.168.0.1 -p [t-0 . t-9]\n"
+		print " -t [target]         IP or DNS target "
+		print " -p [profile]        type of scan [t-0,t-1,t-2,...,t-9]"
+		CLASS_LINKER.space()
+	else:
+		PortScanner.run(target=t, types=p)
+elif m == "set/facebrok":
+	if h==1:
+		print "\n python "+KTFLINKER+" -m set/facebrok -u root -p toor -c db_fbrok -x admin -z admin\n"
+		print " -u [username]         username Mysql"
+		print " -p [password]         password Mysql"
+		print " -c [database]         database Mysql"
+		print " -x [username]         username Panel"
+		print " -z [password]         password Panel"
+		CLASS_LINKER.space()
+	else:
+		facebrok.run(username=u,password=0,database=c,userp=x,passp=z)
+
+#ARPPoisoning.run("192.168.1.0","192.168.1.254","wlan0")
 #BruteZIP.run("core/db/test.zip","core/db/pass.dicc")
 #BruteRAR.run("core/db/test.rar","core/db/pass.dicc")
 #WpaBTF.run("core/test/test.cap","E8:40:F2:32:37:FD","core/db/pass.dicc")
-#LANScanner.run("192.168.1.0")
-#facebrok.run("root","toor","db_fbrok","admin","admin")
-#ARPPoisoning.run("192.168.1.0","192.168.1.254","wlan0")
 
 elif m == "":
 	print ""
@@ -150,12 +223,12 @@ elif m == "":
 	print "  Katana framework Linker> "
 	print "  Sub-tool for call module pass parameters diretly. "
 	print "  each module contain a function (run fuction) that "
-	print "  received this parameters for execution of module "
+	print "  received this parameters for execution of module. "
 	print ""
 	print " Example:"
 	print ""
 	print "  ktf.linker -m category/module -h host -p port -d data"
 	print ""
-	print " For more information visit https://github.com/redtoor/katana "
+	print " For more information visit https://github.com/redtoor/katana/doc "
 	print ""
 
