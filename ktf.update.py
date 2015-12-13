5da8f81e582b569efe604d42b3f60f593843bd3e
RedToor <09890@hotmail.com>
2015-12-13 15:56:06 +0000

    Katana update file create

---
+++ ktf.update.py
@@ -0,0 +1,8 @@
+### Katana Framework Update
+### you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation, either version 3 of the License, or
+### (at your option) any later version. 
+
+from core import updatekatana
+updatekatana.update()
