76120cc1be8b3c686aa9062687258bcd3e98359a
RedToor <09890@hotmail.com>
2016-01-19 23:18:42 -0500

    Update info.py

--- core/info.py
+++ core/info.py
@@ -4,14 +4,8 @@
 #
 
 version="0.0.0.8"
-<<<<<<< HEAD
 date="06/12/2015 - 14/01/2016"
 modules="31"
 tema="Warrior theme"
 build="0053"
-=======
-date="29/03/2015 - 06/12/2015"
-modules="30"
-tema="Warrior theme"
-build="0052"
->>>>>>> origin/master
+
