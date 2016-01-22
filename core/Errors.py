61b2d7b30dc71d56ad7d14839c40aa399678941e
RedToor <09890@hotmail.com>
2016-01-22 09:58:53 -0500

    Mysql Errors added

--- core/Errors.py
+++ core/Errors.py
@@ -10,10 +10,16 @@ d=DESIGN()
 def Errors(event, info):
 	#DEBUG
 	#print event, info
-
+	info=str(info)
 	string=str(event)
 	if string.find("IOError") >= 0:
 		return d.no_file_found(str(info))
+	if string.find("OperationalError(1045") >= 0:
+		d.No_match()
+		return
+	if string.find("_mysql_exceptions.OperationalError") >= 0:
+		print ' '+Bad+' Host '+info+' is not allowed to connect to this MySQL server.\n'
+		return
 	if string.find("socket") >= 0:
 		return d.target_off(str(info))
 	if string.find("KeyboardInterrupt") >= 0 and info!=False:
