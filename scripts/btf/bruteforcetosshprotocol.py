5d99c845f70e271ccaf33d26d7b0aa04170d324d
RedToor <09890@hotmail.com>
2016-04-19 13:57:17 -0400

    Update bruteforcetosshprotocol.py

--- scripts/btf/bruteforcetosshprotocol.py
+++ scripts/btf/bruteforcetosshprotocol.py
@@ -18,7 +18,7 @@ def initialize():
 	initialize.Author             ="RedToor"
 	initialize.Version            ="1.1"
 	initialize.Despcription       ="Brute Force to SSH protocol."
-	initialize.CodeName           ="btf/pc.ssh"
+	initialize.CodeName           ="btf/pr.ssh"
 	initialize.DateCreation       ="07/03/2015"      
 	initialize.LastModification   ="25/03/2016"
 
