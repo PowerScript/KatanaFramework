105c6490d11e6cbf6c515ed8225c77676557f802
RedToor <09890@hotmail.com>
2016-05-02 23:23:55 -0400

    Update splash.py

--- core/splash.py
+++ core/splash.py
@@ -2,31 +2,29 @@
 #HEADER#######################
 # Katana framework           #
 # Splash File                #
-# Last Modified: 27/03/2016  #
+# Last Modified: 01/05/2016  #
 # Review: 0                  #
 #######################HEADER#
 
 from core import colors
 from core import info
 print colors.B+"""
-
-
-          mM@@MM@@MM@@MM@@MM@@MM@@@MMM@@MM@@@@Mm
-         mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm
-         @MMMM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMM"""+colors.B+"""MM"""+colors.W+"""MMMMMMMMM"""+colors.B+"""MMM"""+colors.W+"""MMMMMMM"""+colors.B+"""MMMMm
-         MMMMM"""+colors.W+"""MMM"""+colors.B+"""MM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMMMMMMMM"""+colors.B+"""MMM"""+colors.W+"""MMMMMMM"""+colors.B+"""MMMM@
-         @MMMM"""+colors.W+"""MMMMMM"""+colors.B+"""MMMMMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMMMMM
-         MMMMM"""+colors.W+"""MMM"""+colors.B+"""MM"""+colors.W+"""MMMM"""+colors.B+"""MMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMMMMMM"""+colors.B+"""MMMM@
-         @MMMM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMM"""+colors.B+"""MMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMMMMMM"""+colors.B+"""MMMMM
-         MMMMM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMM"""+colors.B+"""MMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMMMM@
-         @MMMM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMM"""+colors.B+"""MMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMMMMM
-         """+colors.HH+"""mMMMMMMMMMMMMMMM"""+colors.W+colors.GE+"""FRAMEWORK"""+colors.W+colors.HH+colors.B+"""MMMMMMMMMMMMMMm
-        .-.--.@MM@@MM@@MM@@MM@@MM@@MM@@MM@@Mm"""+colors.W+"""
-	\  \==\ 
-	/' /==/ """+colors.HH+""" Core    """+colors.W+""" [ """+info.version+""", Build: """+info.build+""" ]
-	|,|==|  """+colors.HH+""" Date    """+colors.W+""" [ """+info.date+""" ]
-	\  \==\ """+colors.HH+""" Theme   """+colors.W+""" [ """+info.tema+""" ]
-	 \ ,\==\ """+colors.HH+""" Modules """+colors.W+""" [ """""+info.modules+""" Scritp's"""+colors.W+""" ]
-	 / -/==/"""+colors.GW+""" By RedToor"""+colors.W+"""
-	 `-'--'  
-	  """
+                   mM@@MM@@MM@@MM@@MM@@MM@@@MMMMM@@@@Mm              
+       ||========mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm===========||        
+       ||        @MMMM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMM"""+colors.B+"""MM"""+colors.W+"""MMMMMMMMM"""+colors.B+"""MMM"""+colors.W+"""MMMMMMM"""+colors.B+"""MMMMm         ||
+       ||        MMMMM"""+colors.W+"""MMM"""+colors.B+"""MM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMMMMMMMM"""+colors.B+"""MMM"""+colors.W+"""MMMMMMM"""+colors.B+"""MMMM@         ||
+       ||        @@MMM"""+colors.W+"""MMMMMM"""+colors.B+"""MMMMMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMMMMM         ||
+       ||        @MMMM"""+colors.W+"""MMMMM"""+colors.W+"""MMMM"""+colors.B+"""MMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMMMMMM"""+colors.B+"""MMMM@         ||
+       ||        @@MMM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMM"""+colors.B+"""MMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMMMMMM"""+colors.B+"""MMMMM         ||
+       ||        @MMMM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMM"""+colors.B+"""MMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMMMM@         ||
+       ||========@MMMM"""+colors.W+"""MMM"""+colors.B+"""MMM"""+colors.W+"""MMM"""+colors.B+"""MMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMM"""+colors.W+"""MMM"""+colors.B+"""MMMMMMMMM=========||
+       ||         """+colors.HH+""""""+colors.W+"""/TT\\"""+colors.HH+colors.B+"""mMMMMMMMMMM"""+colors.W+colors.HH+"""FRAMEWORK"""+colors.W+colors.HH+colors.B+"""MMMMMMMMMMMMm           ||
+       ||        """+colors.W+"""(____)"""+colors.HH+colors.B+"""@MM@@MM@@MM@@MMMMMM@@MM@@Mm"""+colors.W+"""                ||
+       ||        |  B |                                           ||
+       ||        |  O | """+colors.HH+""" Core    """+colors.W+""" [ """+info.version+""", Build: """+info.build+""" ]        ||
+       ||        |  M | """+colors.HH+""" Date    """+colors.W+""" [ """+info.date+"""    ]        ||
+       ||        |__B_| """+colors.HH+""" Date    """+colors.W+""" [ """+info.date+"""    ]        ||
+       ||        |_  _| """+colors.HH+""" Theme   """+colors.W+""" [ """+info.tema+"""        ]        ||        
+       ||        /|__|\\  """+colors.HH+"""Modules """+colors.W+""" [ """""+info.modules+""" Scritp's"""+colors.W+"""          ]        ||
+       """+colors.GW+"""||       /__\/__\\"""+colors.GW+""" By RedToor                               ||"""+colors.W+"""
+                 ()  ()\n"""
