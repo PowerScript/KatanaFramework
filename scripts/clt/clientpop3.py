5fb3a4ef776c722db551080fca72f481fe290fc3
RedToor <09890@hotmail.com>
2016-05-07 03:04:53 -0400

    Update clientpop3.py

--- scripts/clt/clientpop3.py
+++ scripts/clt/clientpop3.py
@@ -10,23 +10,23 @@ import sys                    #
 Message=DESIGN()              #
 # :-:-:-:-:-:-:-:-:-:-:-:-:-: #
 # Libraries                   #
-import MySQLdb                #
+import poplib                 #
 # :-:-:-:-:-:-:-:-:-:-:-:-:-: #
 
 # INFORMATION MODULE
 def initialize():
 	initialize.Author             ="RedToor"
 	initialize.Version            ="1.1"
-	initialize.Despcription       ="Console Client for POP3 Protocol."
+	initialize.Description        ="Console Client for POP3 Protocol."
 	initialize.CodeName           ="clt/cl.pop"
 	initialize.DateCreation       ="22/05/2015"      
-	initialize.LastModification   ="03/06/2016"
+	initialize.LastModification   ="07/06/2016"
 
 	# DEFAULT VARIABLES             VALUE                  NAME        RQ     DESCRIPTION
-	initialize.DEFAULT_VARIABLE   =[[LOCAL_IP            , "target" , "yes" , "IP or DNS"]]         #[0][0]
-	initialize.DEFAULT_VARIABLE  +=[[POP_PORT            , "port"   , "no"  , "Service port"]]      #[1][0]
-	initialize.DEFAULT_VARIABLE  +=[[USERNAME            , "user"   , "yes" , "Username target"]]   #[2][0]
-	initialize.DEFAULT_VARIABLE  +=[[PASSWORD            , "pass"   , "yes" , "Password target"]]   #[3][0]
+	initialize.DEFAULT_VARIABLE   =[[LOCAL_IP           , "target" , "yes" , "IP or DNS"]]         #[0][0]
+	initialize.DEFAULT_VARIABLE  +=[["995"              , "port"   , "no"  , "Service port"]]      #[1][0]
+	initialize.DEFAULT_VARIABLE  +=[["mem@hotmail.com"  , "user"   , "yes" , "Username target"]]   #[2][0]
+	initialize.DEFAULT_VARIABLE  +=[["password"         , "pass"   , "yes" , "Password target"]]   #[3][0]
 initialize()
 # END INFORMATION MODULE
 
@@ -44,55 +44,41 @@ def main(run):
 			Message.run()
 			# CODE MODULE    ############################################################################################
 			try:
-				getFunction.live(initialize.DEFAULT_VARIABLE[0][0],initialize.DEFAULT_VARIABLE[1][0])
-				if True:
-					try:
-						con=poplib.POP3(initialize.DEFAULT_VARIABLE[0][0], initialize.DEFAULT_VARIABLE[1][0])
-						if True:
-							try:
-								con.user(initialize.DEFAULT_VARIABLE[2][0])
-								con.pass_(initialize.DEFAULT_VARIABLE[3][0])	
-								cmd="nop"
-								print "\n "+Hlp+" POP3 Client help"
-								print " -------------------------------------------------------------------------------------------------------"
-								print " |"+colors[6]+"Command"+colors[0]+"|"+colors[6]+"Description"+colors[0]+"|"+colors[6]+"Examples"+colors[0]+"|"
-								print " -------------------------------------------------------------------------------------------------------"
-								print " |list	| list mails  | list             |" 
-								print " |retr	| show mail   | retr 2           |"
-								print " |dele	| remove mail | dele 2           |"
-								print " ---------------------------------------\n"
-								while(cmd!="exit"):
-									cmd = raw_input(d.Client_prompt('pop3'))
-									if cmd == "list":
-										numMessages = len(red.list()[1])
-										for i in range(numMessages):
-										    print "	mail "+str(i)
-									if cmd[0:4] == "retr":
-										for j in red.retr(int(cmd[5:])+1)[1]:
-											print j
-									if cmd[0:4] == "dele":
-										try:
-										    red.dele(int(cmd[5:])+1)[1]
-										    if True:
-										    	print " "+War+" email marked for delete ('quit' for exit and delete all email marked)"
-										except Exception,e:
-											 print(" "+Bad+" Error", e)
-									if cmd == "quit":
-										red.quit()
-										print " "+Alr+" Exit, bye."
-										break
-							except:
-								Errors.Errors(event=sys.exc_info(), info=False)
-					except:
-						Errors.Errors(event=sys.exc_info(), info=initialize.DEFAULT_VARIABLE[2][0]+":"+initialize.DEFAULT_VARIABLE[3][0])
-			except:
-				Errors.Errors(event=sys.exc_info(), info=initialize.DEFAULT_VARIABLE[0][0]+":"+initialize.DEFAULT_VARIABLE[1][0])			# END CODE MODULE ############################################################################################
+				con=poplib.POP3_SSL(initialize.DEFAULT_VARIABLE[0][0], initialize.DEFAULT_VARIABLE[1][0])
+				con.user(initialize.DEFAULT_VARIABLE[2][0])
+				con.pass_(initialize.DEFAULT_VARIABLE[3][0])
+				cmd="nop"
+				print "\n "+Hlp+" POP3 Client help"
+				print " -----------------------------------------"
+				print " |"+colors[6]+"Command"+colors[0]+"|"+colors[6]+"Description "+colors[0]+" |"+colors[6]+"Examples"+colors[0]+"         |"
+				print " -----------------------------------------"
+				print " |list	 | list mails  | list            |" 
+				print " |read	 | show mail   | retr 2          |"
+				print " |dele	 | remove mail | dele 2          |"
+				print " -----------------------------------------\n"
+				while(cmd!="exit"):
+					cmd = raw_input(Message.Client_prompt('pop3'))
+					if cmd == "list":
+						numMessages = len(con.list()[1])
+						for i in range(numMessages):print " E-mail "+str(i)
+					if cmd[0:4] == "read":
+						for j in con.retr(int(cmd[5:])+1)[1]:
+							print j
+					if cmd[0:4] == "dele":
+						try:
+						    con.dele(int(cmd[5:])+1)[1]
+						    print " "+Alr+" email marked for delete ('quit' for exit and delete all email marked)"
+						except Exception,e:print(" "+Bad+" Error", e)
+					if cmd == "quit":
+						con.quit()
+						break
+
+			except poplib.error_proto as Error:print " "+Bad+" "+Error.message
+			except:Errors.Errors(event=sys.exc_info(), info=sys.exc_traceback.tb_lineno)
 			# END CODE MODULE ############################################################################################
-		else:
-			getFunction.KatanaCheckActionGlobalCommands(actions)
+		else:getFunction.KatanaCheckActionGlobalCommands(actions)
 	# ERROR GENERAL
-	except:
-		Errors.Errors(event=sys.exc_info(), info=sys.exc_traceback.tb_lineno)
+	except:Errors.Errors(event=sys.exc_info(), info=sys.exc_traceback.tb_lineno)
 	# END ERROR GENERAL
 	main(True)
 # END MAIN FUNCTION
