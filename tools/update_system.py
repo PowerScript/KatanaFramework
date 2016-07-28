088f0f0901c0a20865aae132884dd24e10e4e6da
RedToor <redtoor@inbox.ru>
2016-07-28 11:25:42 -0500

    Upgrade to Build 0062

--- tools/update_system.py
+++ tools/update_system.py
@@ -1,1 +1,21 @@
+# This Tool requires katana framework 
+# https://github.com/PowerScript/KatanaFramework
 
+# :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-: #
+# Katana Core import                  #
+from core.KATANAFRAMEWORK import *    #
+# :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-: #
+
+# LIBRARIES  
+from os import system as sysc
+# END LIBRARIES 
+
+class init:
+	
+	Author      = "0xicl33n"
+	Description = "Update Debian based system"
+	var         = {}
+	Arguments   = {'y'[True,'Assume yes and proceed with update']}
+
+def Main():
+	sysc('sudo apt-get update && sudo apt-get %s upgrade && sudo apt-get clean && sudo apt-get dist-upgrade %s') % init.var['y']
