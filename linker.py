bcd5bc4cdef8c52e69fedb3a1d0958e570992c9e
RedToor <redtoor@inbox.ru>
2016-09-21 15:32:36 -0500

    Update

---
+++ linker.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#HEAD#########################################################
+#
+# Katana Framework | linker                            
+# Last Modified: 15/09/2016
+#
+#########################################################HEAD#
+
+
+from core.Function import (GetRootModules)
+
+import importlib
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--module", help=" Script module to run.")
+args = parser.parse_args()
+
+
+if __name__=="__main__":
+	root=GetRootModules()
+	for modules in root.findall('module'):
+		if args.module == modules.get('name'):
+			category = modules.find('category').text
+			filename = modules.find('filename').text
+			ModuleToStart = importlib.import_module("modules."+category+"."+filename) 
+			init=ModuleToStart.init()
+			output = ""
+			for VAR in init.options:
+				output += VAR+":_:"+str(init.options[VAR][0])+":_:"+str(init.options[VAR][1])+":_:"+str(init.options[VAR][2])+":_:_:"
+	 		
+			print output[:-5]
+	
+
+
+
+
+
+
