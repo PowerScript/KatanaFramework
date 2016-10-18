398bb49a4f0bc72212646a28afe7a32a90ef5d67
RedToor <09890@hotmail.com>
2016-10-17 22:42:23 -0500

    Update Information.py

--- core/Information.py
+++ core/Information.py
@@ -2,10 +2,17 @@
 #HEADER#######################
 # Katana framework           #
 # Information File           #
-# Last Modified: 24/05/2016  #
+# Last Modified: 17/10/2016  #
 #######################HEADER#
 
-version="0.0.1.0"
-date="21/09/16:08/10/16"
-build="0065"
-Type="FREE"
+__nickname__   = 'Katana'
+__author__     = 'RedToor'
+__emailadd__   = 'redtoor@inbox.ru'
+__twitter__    = 'https://twitter.com/redtoor'
+__facebook__   = 'https://facebook.com/redtoor'
+__license__    = 'GPLv3'
+
+version        = "0.0.1.0"
+date           = "21/09/16:08/10/16"
+build          = "0065"
+Type           = "FREE"
