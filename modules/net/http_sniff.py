cce8e76180b02502659ec3fa90f49e64a4e1036d
ThomasTJ <thomastjdev@gmail.com>
2016-11-26 18:01:02 +0100

    Fixed wrong if-condition. yes should be true.

--- modules/net/http_sniff.py
+++ modules/net/http_sniff.py
@@ -1,4 +1,4 @@
-# This module requires katana framework 
+# This module requires katana framework
 # https://github.com/PowerScript/KatanaFramework
 
 # :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-: #
@@ -6,11 +6,11 @@
 from core.KATANAFRAMEWORK import *	  #
 # :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-: #
 
-# LIBRARIES 
+# LIBRARIES
 from core.Function import get_interfaces,checkDevice,get_monitors_mode
-from scapy.all import *	
-import sys, re, time	
-# END LIBRARIES 
+from scapy.all import *
+import sys, re, time
+# END LIBRARIES
 
 # VARIABLES
 gcookie = "" 	# Global for not printing the same cookie multiple times
@@ -23,7 +23,7 @@ def init():
 	init.Version			="1.0"
 	init.Description		="HTTP sniffer"
 	init.CodeName			="net/http.sniff"
-	init.DateCreation		="09/11/2016"	  
+	init.DateCreation		="09/11/2016"
 	init.LastModification	="09/11/2016"
 	init.Collaborators      =None
 	init.References		 	=None
@@ -38,9 +38,9 @@ def init():
 		'onlycreds'	:["false"			,False,'Only show creds'],
 		'hideempty'	:["true"			,False,'Hide empty pkts'],
 		'ignore'	:["false"			,False,'Ignore js etc']
-		
+
 	}
-	
+
 	init.aux =  "\n  {PARAMETER}  {DESCRIPTION} -> [filter]"
 	init.aux += "\n  [ALL]        Whatever"
 	init.aux += "\n  [DNS]        Domains Name Service"
@@ -57,7 +57,7 @@ def init():
 
 # CODE MODULE	############################################################################################
 def main(run):
-	
+
 	try:
 		if  init.var['filter'] == "DNS" : FILTER = "udp or port 53"
 		elif init.var['filter']  == "FTP" : FILTER = "port 21"
@@ -69,7 +69,7 @@ def main(run):
 			printAlert(1,"Type not allow, use show options or sop and see Auxiliar help.")
 			FILTER = "udp or tcp"
 			return
-		
+
 		if checkDevice(init.var['interface']):
 			print " "+colors[4]+("TIME: %-*s ID:    PRO:%-*s SRC: %-*s DST: %-*s PORT: %-*s HOST:  TYPE:  PATH:" % (4, "", 3, "", 16, "", 16, "", 5, ""))+colors[0]
 			while True:sniff(filter=FILTER, prn=callback, store=0, iface=init.var['interface'])
@@ -82,7 +82,7 @@ def main(run):
 
 
 def callback(pkt):
-	
+
 	#try:
 	if pkt.dport == 53 and pkt[DNS].opcode == 0L and pkt[IP].proto == 17:
 		printable = (
@@ -90,19 +90,19 @@ def callback(pkt):
 			+ ("%-*s" % (6, str(pkt[IP].id)))+colors[13]+"  DNS   "+colors[0]+(" SRC: %-*s DST: %s" % (16, str(pkt[IP].src), pkt[DNS].qd.qname))+colors[0]
 			)
 		return printable
-	
-	
+
+
 	# MAIL
 	mailuserpass = ""
 	if pkt.dport == 25 or pkt.dport == 110 or pkt.dport == 143:
 		if pkt[TCP].payload:
 			ftp_packet = str(pkt[TCP].payload)
 			# Only interested in USER and PWD
-			if init.var['onlycreds'] == "yes":
+			if init.var['onlycreds'] == "true":
 				if 'user' in ftp_packet.lower() or 'pass' in ftp_packet.lower():
 					printable = " ["+ time.strftime("%H:%M:%S")+"] "+("%-*s" % (6, str(pkt[IP].id)))+colors[11]+"  POP  "+colors[0]+" "+("SRC: %-*s DST: %-*s" % (16, str(pkt[IP].src), 16, str(pkt[IP].dst)))+" "+"DATA:  "+colors[2]+str(pkt[TCP].payload).replace("\n", ".")+colors[0]
 					return printable
-			
+
 			if 'user' in ftp_packet.lower() or 'pass' in ftp_packet.lower():
 				mailuserpass = ("DATA:  " + colors[2] + str(pkt[TCP].payload).replace("\n", " "))
 			elif ftp_packet:
@@ -112,17 +112,17 @@ def callback(pkt):
 					mailuserpass = ("DATA:  " + str(pkt[Raw].load).replace("\n", " "))
 				except:
 					mailuserpass = ""
-				
+
 			printable = " ["+ time.strftime("%H:%M:%S")+"] "+("%-*s" % (6, str(pkt[IP].id)))+colors[11]+"  POP  "+colors[0]+" "+("SRC: %-*s DST: %-*s" % (16, str(pkt[IP].src), 16, str(pkt[IP].dst)))+" "+mailuserpass+colors[0]
 			return printable
-	
+
 	# FTP
 	userpass = ""
 	if pkt.dport == 21:
 		if pkt[TCP].payload:
 			ftp_packet = str(pkt[TCP].payload)
 			# Only interested in USER and PWD
-			if init.var['onlycreds'] == "yes":
+			if init.var['onlycreds'] == "true":
 				if 'user' in ftp_packet.lower() or 'pass' in ftp_packet.lower():
 					printable = " ["+ time.strftime("%H:%M:%S")+"] "+("%-*s" % (6, str(pkt[IP].id)))+colors[12]+"  FTP   "+colors[0]+" "+("SRC: %-*s DST: %-*s" % (16, str(pkt[IP].src), 16, str(pkt[IP].dst)))+" "+"DATA:  "+colors[2]+str(pkt[TCP].payload).replace("\n", ".")+colors[0]
 					return printable
@@ -137,10 +137,10 @@ def callback(pkt):
 						userpass = ("DATA: " + str(pkt[Raw].load).replace("\n", " "))
 					except:
 						userpass = ""
-				
+
 				printable = " ["+ time.strftime("%H:%M:%S")+"] "+("%-*s" % (6, str(pkt[IP].id)))+colors[12]+"  FTP   "+colors[0]+" "+("SRC: %-*s DST: %-*s" % (16, str(pkt[IP].src), 16, str(pkt[IP].dst)))+" "+userpass+colors[0]
 				return printable
-	
+
 	# HTTP
 	host2 = "" #test
 	payload = ""
@@ -168,10 +168,10 @@ def callback(pkt):
 			raw_dport = str(pkt[TCP].dport)
 		except:
 			raw = ""
-			
+
 		if raw == "":
 			return None
-			
+
 		# Get username
 		username = ""
 		if 'user' in raw:
@@ -189,14 +189,14 @@ def callback(pkt):
 					password = str(mp.group(1))
 			if password.isspace():
 				password = ""
-		
+
 		# Get path
 		if raw:
 			mpath = re.search('\\\\r\\\\n\\\\r\\\\n([A-Za-z0-9%\.=&_-]+)', raw) #b(.+[A-Za-z0-9%_-])\\\\r\\\\nHost:
 			if mpath:
 				path = "  PATH: " + str(mpath.group(1))
-		
-		# Get cookie				
+
+		# Get cookie
 		if raw:
 			mcookie = re.search('Cookie:\s([A-Za-z0-9%=&_-]+)\\\\r\\\\n', raw) #b(.+[A-Za-z0-9%_-])\\\\r\\\\nHost:
 			if mcookie:
@@ -215,8 +215,8 @@ def callback(pkt):
 						gcookie = cookie
 			# Do a check for stupid cookies and ignore them
 			#if 'Gdyn' or 'gscroll' in cookie:
-			#	cookie = ""	
-			
+			#	cookie = ""
+
 		# Get host
 		if raw:
 			mhost = re.search('Host:\s([A-Za-z0-9%\.=&_-]+)\\\\r\\\\n', raw) #b(.+[A-Za-z0-9%_-])\\\\r\\\\nHost:
@@ -232,7 +232,7 @@ def callback(pkt):
 				mpost = re.search('(GET.*[A-Za-z0-9%_-]+).HTTP', raw)
 				if mpost:
 					post = "  TYPE: " + str(mpost.group(1))
-			
+
 		# Get secret
 		if raw:
 			msecret = re.search('([A-Za-z0-9%=&_-]+secret[A-Za-z0-9%=&_-]+)', raw, re.IGNORECASE) #b(.+[A-Za-z0-9%_-])\\\\r\\\\nHost:
@@ -242,16 +242,16 @@ def callback(pkt):
 					secret = ""
 				else:
 					gsecret = secret
-		
-		# Get CSRF		
+
+		# Get CSRF
 		if raw:
 			mcsrf = re.search('csrf[A-Za-z0-9%_-]*=([A-Za-z0-9%_-]+)', raw, re.IGNORECASE)
 			if mcsrf:
 				csrf = "  CSRF: " + str(mcsrf.group(1))
-				
-		
+
+
 		if password:
-			if init.var['onlycreds'] != "yes":
+			if init.var['onlycreds'] != "true":
 				printablecon = (
 					'\n'
 					+ ' ' + colors[3] + '['+ time.strftime("%H:%M:%S")+']' + colors[0] + ' ' + colors[2] + 'CREDS CATCHED:' + colors[0]
@@ -269,13 +269,13 @@ def callback(pkt):
 					+ '\n\t\t  SECRET:   ' + secret.replace("  SECRET: ", "")
 					+ '\n\n'
 					)
-		
+
 			# Only CREDS
-			if init.var['onlycreds'] == "yes":
+			if init.var['onlycreds'] == "true":
 				printablecon = (
 					'\n'
 					+ ' ' + colors[3] + '['+ time.strftime("%H:%M:%S")+']' + colors[0] + ' ' + colors[2] + "CREDS CATCHED:" + colors[0]
-					+ '\n ' + " ["+ time.strftime("%H:%M:%S")+"] " + str(pkt[IP].id) 
+					+ '\n ' + " ["+ time.strftime("%H:%M:%S")+"] " + str(pkt[IP].id)
 					+ '\n'
 					+  colors[2] + '\n\t\t  ORIGIN  : ' + str(pkt[IP].src) + colors[0]
 					+  colors[2] + '\n\t\t  USERNAME: ' + username + colors[0]
@@ -283,22 +283,25 @@ def callback(pkt):
 					+ '\n\t\t  Path:	 ' + path
 					+ '\n'
 					)
-					
+
 			return printablecon
-			
+
+		elif init.var['onlycreds'] == "true":
+			return None
+
 		elif cookie or secret or csrf:
 			return ' '+colors[3]+'['+ time.strftime("%H:%M:%S")+']'+colors[0]+" "+("%-*s  Other  SRC: %-*s DST: %-*s PORT: %-*s" % (6, str(pkt[IP].id),16, str(pkt[IP].src), 16, str(pkt[IP].dst), 5, raw_dport))+host+post+path+colors[2]+cookie+secret+csrf+colors[0]
-		
+
 		elif 'login' in post.lower():
 			return ' '+colors[3]+'['+ time.strftime("%H:%M:%S")+']'+colors[0]+" "+("%-*s  Other  SRC: %-*s DST: %-*s PORT: %-*s" % (6, str(pkt[IP].id),16, str(pkt[IP].src), 16, str(pkt[IP].dst), 5, raw_dport))+host+colors[2]+post+path+colors[0]
-		
+
 		else:
 			if init.var['ignore'] == "yes":
 				# Create for loop checking each user input ignore files instead of static
 				#if re.search('(\.jpg)', post, re.IGNORECASE) is not None or re.search('(\.js)', post, re.IGNORECASE) is not None or re.search('(\.css)', post, re.IGNORECASE) is not None:
 				if re.search('(\.jpg|\.js|\.css|\.jpeg|\.svg|\.png)', post, re.IGNORECASE) is not None:
-					return None 
-			
+					return None
+
 			if init.var['hideempty'] == "yes":
 				#if raw != "" or payload != "":
 				if post != "":
@@ -307,4 +310,3 @@ def callback(pkt):
 					return None
 			else:
 				return colors[9]+" ["+ time.strftime("%H:%M:%S")+"] "+("%-*s  Other  SRC: %-*s DST: %-*s PORT: %-*s" % (6, str(pkt[IP].id), 16, str(pkt[IP].src), 16, str(pkt[IP].dst), 5, raw_dport))+host+post+path
-		
