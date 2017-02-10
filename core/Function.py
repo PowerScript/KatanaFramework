31e93b30d5e8a04433ffb562113e09e08992288b
R̭̮̗̯̳̯̜̳̱̰̮͍̯̯̲̝̥̔ͯ͆̆͌̈͌̽͒̿ͯͤ͂̚ͅͅė̥̜̗͉̦̰͈̺̟̻̦̔ͬͧ̍ͣ̉̚d̦̦̯̩̠̗͔̜̼̬̏ͫ̌̓́ͦ̆̋̅ͯ̌͆͂͑̊̑̄̊T̖̱͕̬̬͈̹̏̆ͮ̽́ͫ̎oor <09890@hotmail.com>
2017-02-10 17:17:13 -0600

    Update Function.py

--- core/Function.py
+++ core/Function.py
@@ -359,7 +359,8 @@ class SYSTEM:
 			i = "["+colors[1]+"\033[1mERROR"+colors[0]+"]"+colors[0]+"["+colors[3]+"\033[1mWARNING"+colors[0]+"]"
 
 		stdout.write("\r" + " " + information + " " + msg +" STATUS:%s                      \r" % i)
-
+		stdout.write("\n")
+		
 	def Rtask(self,process):
 		xtem="" 
 		if XTERM_OPTION:xtem="xterm -e "
